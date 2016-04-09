b3c4aaa12eaae7c47aba524c6f1e64f0295e9740
yole <424188487@qq.com>
2016-04-09 16:19:39 +0800

    index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,68 @@
 <template>
-    <div>index</div>
+  <div class="container">
+    <img class="pic" alt="" :src="img"
+         src='https://pic3.zhimg.com/2d16f25c61e0323babf2f8ff5eb94d9f.jpg'>
+    <label class="lb">{{text}}</label>
+  </div>
 </template>
+
+<script>
+  import * as api from '../api'
+  export default {
+    data () {
+      return {
+        img: '',
+        text: ''
+      }
+    },
+    ready () {
+      let vm = this
+      this.getData().then(function (data) {
+        var ob = data.data
+        vm.img = ob.img
+        vm.text = ob.text
+        vm.$el.querySelector('img').style.transition = 'all 3.5s'
+        vm.$el.querySelector('img').style.width = '110%'
+        vm.$el.querySelector('img').style.height = '110%'
+//        vm.$el.querySelector('img').style.transition = 'all 2s 2'
+//        vm.$el.querySelector('img').style.opacity = '0'
+      })
+
+      setTimeout(function () {
+        vm.$route.router.go({path: 'newsList'})
+      }, 4000)
+    },
+    methods: {
+      getData () {
+        return api.getStartImg()
+      }
+    }
+  }
+</script>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+</style>
+<style scoped>
+  .container {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    position: relative;
+  }
+  .pic {
+    width: 100%;
+    height: 100%;
+  }
+  .lb {
+    height: 20px;
+    width: 100%;
+    position: absolute;
+    left: 0.1px;
+    bottom: 20px;
+    color: #9da0a4;
+  }
+</style>
