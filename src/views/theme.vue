cdd19159ebb92222a6b5fd3882c758ecc07c5127
yole <424188487@qq.com>
2016-05-31 13:50:06 +0800

    add theme content

--- src/views/theme.vue
+++ src/views/theme.vue
@@ -3,9 +3,10 @@
       <span class="icon">←</span>
       <span class="title">{{name}}</span>
   </div>
-  <div class="">
-
-  </div>
+  <section v-for="item in stories" v-link="'/news/'+ item.id">
+    <h3>{{item.title}}</h3>
+    <img :src="item.images[0]" alt="标题图片">
+  </section>
 </template>
 
 <script>
@@ -59,4 +60,26 @@
   .banner .title {
   }
 
+  section {
+    height: 5em;
+    background-color: #ffffff;
+    border: 1px solid #F1E5E5;
+    border-radius: 5px;
+    margin: 10px;
+    padding: 15px;
+    position: relative;
+  }
+  section h3 {
+    width: 70%;
+    float: left;
+    margin: 0;
+    font-size: 1.3em;
+    line-height: 1.2em;
+  }
+  section img {
+    width: 24%;
+    clip: rect(0px,auto,5em,0px);
+    position: absolute;
+    right: 15px;
+  }
 </style>
